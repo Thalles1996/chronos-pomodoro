20b3ebdad4751046ae61a3ca7f1e2d2c78eee9d2
Thales Carvalho <thallesjc1996@gmail.com>
2025-08-29 11:57:37 -0300

    Primeira página do site conluida

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,48 @@
 import './styles/theme.css';
 import './styles/global.css';
-import { Container } from './components/Container'
+
+
+import { Container } from './components/Container';
+import { Logo } from './components/Logo';
+import { Menu } from './components/Menu';
+import { CountDown } from './components/CountDown';
+import { DefaultInput } from './components/DeafultInput';
+
 
 export function App() {
   return (
     <>
       <Container>
-          <section>LOGO</section>
+          <Logo />
       </Container>
 
       <Container>
-          <section>MENU</section>
+          <Menu />
+      </Container>
+
+      <Container>
+          <CountDown />
+      </Container> 
+
+      <Container>
+          <form className='form' action="">
+            <div className='formRow'>
+              <DefaultInput id='meuInput' type='text' />
+            </div>
+
+            <div className='formRow'>
+              <p>Lorem ipsum dolor sit amet.</p>
+            </div>
+
+            <div className='formRow'>
+              <p>Ciclos: </p>
+              <p>0 0 0 0 0 </p>
+            </div>
+
+            <div className='formRow'>
+              <button>Enviar</button>
+            </div>
+          </form>
       </Container>      
     </>
   );
